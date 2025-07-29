45d520a671702654e4b643187ad5a165a6c6b74c
Leonardo Mantovanello <rm95222@estudante.fieb.edu.br>
2025-07-29 23:12:00 +0000

    teste

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App () {
-    return <h1>Pagina inicial</h1>
-}
-
-export default App
---
+++ src/Paginainicial.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function App () {
+    return <h1>Pagina inicia</h1>
+}
+
+export default App
