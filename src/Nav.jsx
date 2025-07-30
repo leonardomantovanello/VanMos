71efad2a673ca3bbe9c0481c952c1ad1b4f9427a
leonardomantovanello <rm95222@estudante.fieb.edu.br>
2025-07-29 21:47:23 -0300

    navbar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,7 +4,7 @@ import './Nav.css'
 const Nav = () => {
     return (
     <header className="header">
-        <a href="/" className="logo">Logo</a>
+        <a href="/" className="logo">VANMOS</a>
 
         <nav className="navbar">
             <a href="/">Pagina Principal</a>
